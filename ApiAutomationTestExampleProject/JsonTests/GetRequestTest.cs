5fae137768500846d8dbf3ff555067dcb38a15dc
Sam Chen <samchen63@users.noreply.github.com>
2019-06-18 14:35:31 +1000

    Update GetRequestTest.cs

--- ApiAutomationTestExampleProject/JsonTests/GetRequestTest.cs
+++ ApiAutomationTestExampleProject/JsonTests/GetRequestTest.cs
@@ -1,11 +1,3 @@
-﻿//-----------------------------------------------------------------------------
-// <copyright file="GetRequestTest.cs" company="Planit Testing">
-//      Copyright © 2019 Planit Testing.
-//      All rights reserved.
-// </copyright>
-// <created>11/04/2019</created>
-// <author>Sam Chen</author>
-//-----------------------------------------------------------------------------
 namespace ApiAutomationTestExampleProject.JsonTests
 {
     using ApiAutomationTestExampleProject.JsonProcessor;
